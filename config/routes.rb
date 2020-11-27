092d38eabcbba301cf8f7e47a137a6fa8584060e
Sunny Ripert <sunny@sunfox.org>
2020-11-27 10:43:14 +0100

    New features. Hurray

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :products, only: [:new, :create]
+
   root to: 'products#index'
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: "George", last_name: "Abitbol")
+    assert_equal "George Abitbol", user.full_name
+
+    user = User.new(first_name: "john", last_name: "lennon")
+    assert_equal "John Lennon", user.full_name
+  end
 end
